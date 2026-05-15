84587d6a23c889262e1e9b58543c2035e071fc67
Skimmerzzz <skimmer@mail.ru>
2026-05-15 18:29:01 +0300

    Implement MenuController. Initial

---
+++ src/ru/yandex/practicum/controller/MenuController.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.controller;
+
+import java.util.Scanner;
+
+public class MenuController {
+    private final Scanner scanner;
+
+    public MenuController() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void showCongratulations(int steps) {
+        System.out.printf("Вы угадали слово за %d попыток!%n", steps);
+    }
+
+    public String readUserAnswer() {
+        String answer = scanner.nextLine();
+        return answer.trim();
+    }
+
+    public void showGreeting() {
+        System.out.println("Добро пожаловать в игру Wordle!");
+        System.out.println("Компьютер «загадывает» слово — " +
+                "существительное в единственном числе в именительном падеже. " +
+                "Используется словарь русских слов, состоящих из пяти букв." +
+                "Используются только буквы (нет пробелов и дефисов). \n" +
+                "Игроку доступно шесть попыток. ");
+    }
+
+    public void showTryAgain(int steps)  {
+        System.out.printf("Осталось %d попыток%n", steps);
+        System.out.println("Попробуйте еще...");
+    }
+}
